b676fce25652138cf1acc7e443571802bdc68732
Humberto Sandmann <hsandmann@ieee.org>
2022-03-10 14:24:52 -0300

    Added lecture 01

---
+++ src/espm/poo/aula01/App.java
@@ -0,0 +1,38 @@
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket server = new ServerSocket(3030);
+        System.out.println("Abriu a porta");
+        while (true) {
+            final Socket cliente = server.accept();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try (Scanner entrada = new Scanner(cliente.getInputStream())) {
+                        String recebido = entrada.nextLine();
+                        System.out.println(cliente.getPort() + ": " + recebido);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    System.out.println("Indo dormir");
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Acordei");
+                }
+            }).start();
+        }
+//        server.close();
+    }
+
+}
---
+++ src/espm/poo/aula01/Cliente.java
@@ -0,0 +1,14 @@
+import java.net.Socket;
+
+public class Cliente {
+
+    public static void main(String[] args) throws Exception {
+
+        Socket cliente = new Socket("127.0.0.1",3030);
+        cliente.getOutputStream().write(args[0].getBytes());
+        cliente.close();
+
+
+    }
+    
+}
